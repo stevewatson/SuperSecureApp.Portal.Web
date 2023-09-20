6c17768e84a7d4c63914e77b6c01c757d53204d7
Steve <steve.watson@msn.com>
2023-09-20 18:01:23 +0100

    Add project files.

---
+++ SuperSecureApp.Portal.Web/Client/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using SuperSecureApp.Portal.Web;
+
+namespace SuperSecureApp.Portal.Web
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddMsalAuthentication(options =>
+            {
+                builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+            });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
