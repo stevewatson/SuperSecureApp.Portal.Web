6e29fe0dca5fee4cbc49b79d3221dfd2a45a743b
Steve <steve.watson@msn.com>
2023-09-26 10:25:03 +0100

    Created access token service, added to DI, added authentication token into HttpClient

--- SuperSecureApp.Portal.Web/Client/Program.cs
+++ SuperSecureApp.Portal.Web/Client/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SuperSecureApp.Portal.Web;
+using SuperSecureApp.Portal.Web.Services;
 
 namespace SuperSecureApp.Portal.Web
 {
@@ -12,7 +14,22 @@ namespace SuperSecureApp.Portal.Web
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            // todosgw: added to allow easy access to tokens throughout the app using DI
+            builder.Services.AddScoped<AccessTokenService>(); 
+            
+            // todosgw: added to allow easy access to httpclient with the additional bearer token header
+            builder.Services.AddScoped(sp =>
+            {
+                var httpClient = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
+                var accessTokenService = sp.GetRequiredService<AccessTokenService>();
+                var accessToken = accessTokenService.GetAccessTokenAsync().Result;
+                if (!string.IsNullOrEmpty(accessToken))
+                {
+                    httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken); ;
+                }
+                return httpClient;
+            });
 
             builder.Services.AddMsalAuthentication(options =>
             {
---
+++ SuperSecureApp.Portal.Web/Services/AccessTokenService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using System.Threading.Tasks;
+
+namespace SuperSecureApp.Portal.Web.Services
+{
+    public class AccessTokenService
+    {
+        public bool TokenAcquired = false;
+        private readonly IAccessTokenProvider _accessTokenProvider;
+
+        public AccessTokenService(IAccessTokenProvider accessTokenProvider)
+        {
+            _accessTokenProvider = accessTokenProvider;
+        }
+
+        public async Task<string> GetAccessTokenAsync()
+        {
+            var tokenResult = await _accessTokenProvider.RequestAccessToken();
+            TokenAcquired = tokenResult.TryGetToken(out var accessToken);
+
+            return accessToken.Value;
+        }
+    }
+}
